e081b10382637de8a0aa7700c75c53311a550d31
bukolabisuga <bukksbisuga@gmail.com>
2018-07-18 04:02:08 +0200

    Add body-parser middleware

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 7777;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('Bukola is one step closer!'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
