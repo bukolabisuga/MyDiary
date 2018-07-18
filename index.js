754e11f8afffdbcde390266a3da07da9d6948dce
bukolabisuga <bukksbisuga@gmail.com>
2018-07-18 04:06:43 +0200

    Set extended to true for body-parser middleware

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 7777;
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.send('Bukola is one step closer!'));
 
