90161fb3efa8f95554ea21bdfa6ea62918cf1c5f
bukolabisuga <bukksbisuga@gmail.com>
2018-07-20 17:17:41 +0200

    Fix heroku port issue

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import bodyParser from 'body-parser';
 import appRouter from './server/routes';
 
 const app = express();
-const port = 9999;
+const port = process.env.PORT || 9999;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'UI')));
 
 app.get('/', (req, res) => res.send('Bukola is one step closer!'));
 
